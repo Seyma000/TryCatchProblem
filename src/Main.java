6eada581117e1f3c6cf3906e7356e097b1271145
Seyma000 <serterseyma254@gmail.com>
2024-03-18 00:34:01 +0300

    Try Catch Problem

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    static int[] numArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    static int getNumber(int index) {
+        try {
+            return numArray[index];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Hata!! Geçersiz indeks. Dizi boyutu: " + numArray.length);
+
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getNumber(1));
+        System.out.println(getNumber(10));
+    }
+}
